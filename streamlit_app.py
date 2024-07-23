f71d088c084edd4f6639ac3638bafd13ee5b6e1b
Darren Waugh <73534655+DarrenWaugh@users.noreply.github.com>
2024-07-23 14:23:38 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,4 +46,5 @@ st.success ('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response.json())
+#st.text(fruityvice_response.json())
+fv_df = st.datafram(data=fruityvice_response.json(), use_container_width=True)
